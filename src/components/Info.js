0025b1c606e05860f673d1a91a8c8f5b07fb16ac
Cecilia de la Parra <delaparracecilia@gmail.com>
2022-11-10 14:53:07 +0100

    email button not showing in mobile fixed?

--- src/components/Info.js
+++ src/components/Info.js
@@ -10,12 +10,12 @@ export default function Info() {
             <img className="profile-photo" src={Photo} alt="Cecilia" />
             <h1 className="profile-name">Cecilia de la Parra</h1>
             <h2 className="profile-job">Photographer</h2>
-            <a href="www.ceciparra.com">Ceci Parra website</a>
-                <a className="link-button" href="https://mail.google.com/" target="blank">
+            <a className="web-link" href="www.ceciparra.com">Ceci Parra website</a>
+            <a className="link-button" href="https://mail.google.com/" target="blank">
                 <button className="mail-button" type="submit">
                     <FontAwesomeIcon icon={faEnvelope} className="mail-icon" />Email
                 </button>
-                </a>
+            </a>
 
         </div>
     )
